92244b83ebd5b605cfe35422f23d1b2d2a542754
rachee46 <rgachee@gmail.com>
2019-01-13 02:41:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+
+credentials = {
+    "serverHost": {
+        "Value": "auth01.demo.gohealthcast.com"
+    },
+    "serverPort": {
+        "Value": 443
+    },
+    "citrixResourceName": {
+        "Value": "dgwin10"
+    },
+    "appMode": {
+        "Value": 1
+    },
+    "citrixCredentials": {}
+}
+
+credentials["citrixCredentials"]["Value"] = []
+
+with open('device_id.txt', 'r') as file:
+    device_id = file.readlines()
+
+for item in device_id:
+    generic_user = "generic" + str(device_id.index(item) + 1)
+    credentials["citrixCredentials"]["Value"].append({
+        "deviceSerialNumber":
+        item.strip(),
+        "username":
+        generic_user,
+        "password":
+        "Bdltko$5"
+    })
+
+print(credentials)
+with open('credentials.txt', 'w') as outfile:
+    json.dump(credentials, outfile)
